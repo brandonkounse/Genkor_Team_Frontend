cc59f19daea7c42fb180bc874fb9761481678a2a
Brandon Kounse <kounse@gmail.com>
2024-06-26 21:14:45 -0500

    cleaned up main file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { TeamComponent } from './team/team.component';
+import { HeaderComponent } from './header/header/header.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TeamComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Genkor_TeamPage';
-}
+export class AppComponent { }
