310b8f1a72eeec0a6b3a995a74ae28c447703eaf
Brandon Kounse <kounse@gmail.com>
2024-07-04 17:14:10 -0500

    removed fetchTeamMembers and moved subscription to component file

--- src/app/team/team.service.ts
+++ src/app/team/team.service.ts
@@ -7,7 +7,6 @@ import { environment } from "../../environments/environment";
 @Injectable({ providedIn: 'root' })
 export class TeamService {
   private apiUrl: string = environment.apiUrl;
-  public teamMembers: any[] = [];
   public teamSplash: any[] = [
     { "Kounse": "/assets/images/kounse.png" },
     { "TheJimmyO": "/assets/images/jimmy.png" },
@@ -20,11 +19,5 @@ export class TeamService {
 
   public getTeamMembers(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/team/members`)
-  }
-
-  public fetchTeamMembers(): void {
-    this.getTeamMembers().subscribe(data => {
-      this.teamMembers = data;
-    })
   }
 }
