753d7b94919f08d992ff1339d4a2f97ecba68ab7
Brandon Kounse <kounse@gmail.com>
2024-07-06 19:34:41 -0500

    created interface for TeamMember and RankedData

---
+++ src/app/team/team.interface.ts
@@ -0,0 +1,21 @@
+export interface TeamMember {
+  name: string,
+  profile_info: {
+    profile_icon_id: string,
+    summoner_level: string,
+  },
+  ranked_stats: {
+    arena: RankedData,
+    ranked_flex_sr: RankedData,
+    ranked_solo_5x5: RankedData,
+  }
+}
+
+interface RankedData {
+  leagueId: string,
+  losses: string,
+  queueType: string,
+  rank: string,
+  tier: string,
+  wins: string,
+}
