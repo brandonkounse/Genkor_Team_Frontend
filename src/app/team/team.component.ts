be7b567b69e755fe151c5d67d10fb044995d6c04
Brandon Kounse <kounse@gmail.com>
2024-07-07 23:15:46 -0500

    refactored to better handling child component data passing

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChildren, QueryList } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { Subscription } from 'rxjs';
 
 import { TeamService } from './team.service';
+import { PlayerComponent } from './player/player.component';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, PlayerComponent],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
 })
 
 export class TeamComponent implements OnInit, OnDestroy {
+  @ViewChildren(PlayerComponent) playerComponents!: QueryList<PlayerComponent>;
   public teamMembers: any[] = [];
   public splashPaths: string[] = []
   public subscription: Subscription | null = null;
+  public selectedPlayerIndex: number | null = null;
 
   constructor(public teamService: TeamService) { }
 
@@ -24,13 +27,16 @@ export class TeamComponent implements OnInit, OnDestroy {
       next: (members) => this.teamMembers = members,
       error: (e) => console.error('Error fetching team members: ', e)
     });
-
-    this.splashPaths = this.teamService.teamSplash.flatMap(obj => Object.values(obj));
+    console.log(this.teamMembers)
   }
 
   ngOnDestroy(): void {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
+  }
+
+  public onSplashClick(index: number): void {
+    this.selectedPlayerIndex = this.selectedPlayerIndex === index ? null : index;
   }
 }
