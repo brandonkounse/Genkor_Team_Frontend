1baeb913e1195e49a3ba168d7eb988e6f8158366
Brandon Kounse <kounse@gmail.com>
2024-07-07 22:38:36 -0500

    changed data structure for splash images. changed getTeamMembers() observable to better reflect TeamMember interface

--- src/app/team/team.service.ts
+++ src/app/team/team.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { map } from "rxjs";
 
 import { environment } from "../../environments/environment";
+import { TeamMember } from "./team.interface";
 
 @Injectable({ providedIn: 'root' })
 export class TeamService {
   private apiUrl: string = environment.apiUrl;
-  public teamSplash: any[] = [
-    { "Kounse": "/assets/images/kounse.png" },
-    { "TheJimmyO": "/assets/images/jimmy.png" },
-    { "InfernalStro": "/assets/images/stro.png" },
-    { "Benjix": "/assets/images/benjix.png" },
-    { "TolkienBlack": "/assets/images/tolkien.png" },
-  ];
+
+  private teamSplash: { [key: string]: string } = {
+    "Kounse": "/assets/images/kounse.png",
+    "The JimmyO": "/assets/images/jimmy.png",
+    "InfernalStro": "/assets/images/stro.png",
+    "Benjix": "/assets/images/benjix.png",
+    "TolkienBlack": "/assets/images/tolkien.png",
+  }
+    ;
 
   constructor(private http: HttpClient) { }
 
-  public getTeamMembers(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/team/members`)
+  public getTeamMembers(): Observable<TeamMember[]> {
+    return this.http.get<TeamMember[]>(`${this.apiUrl}/team/members`).pipe(
+      map(members => members.map(member => ({
+        ...member,
+        splash: this.teamSplash[member.name] || `No splash found for ${member.name}`
+      })))
+    )
   }
 }
